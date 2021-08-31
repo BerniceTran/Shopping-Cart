da201d44c8a9828ebadd3b7fd44a7d11b18e31ad
MohamedA <mohammedmon3m@yahoo.com>
2021-08-31 17:23:29 -0400

    Payment Component

---
+++ src/Payment.js
@@ -0,0 +1,27 @@
+import './Payment.css';
+function Payment(props) {
+    return (
+      <div class="col-50">
+            <h3>Payment</h3>
+            
+            <label for="cname">Name on Card</label>
+            <input type="text" id="cname" name="cardname" placeholder="John More Doe"></input>
+            <label for="ccnum">Credit card number</label>
+            <input type="text" id="ccnum" name="cardnumber" placeholder="1111-2222-3333-4444"></input>
+            <label for="expmonth">Exp Month</label>
+            <input type="text" id="expmonth" name="expmonth" placeholder="September"></input>
+            <div class="row">
+              <div class="col-50">
+                <label for="expyear">Exp Year</label>
+                <input type="text" id="expyear" name="expyear" placeholder="2018"></input>
+              </div>
+              <div class="col-50">
+                <label for="cvv">CVV</label>
+                <input type="text" id="cvv" name="cvv" placeholder="352"></input>
+              </div>
+            </div>
+          </div>
+    );
+  }
+  
+  export default Payment;
